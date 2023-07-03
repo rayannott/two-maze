9b73700481d6bec23f0763bf4fe6a2a6c2e78d1e
Airat <imjobjob@gmail.com>
2023-07-04 00:14:21 +0200

    added more stuff for gui of p1

--- game_gui_p1.py
+++ game_gui_p1.py
@@ -1,18 +1,19 @@
 import pygame
 import numpy as np
+import random
 
 from game import Game
 from mazes import TC, TT, TileItemType
 from gui.gui_rect import Button, Panel
 from gui.gui_utils import *
-from utils.constants import NUM_OF_MAZES
+from utils.constants import NUM_OF_MAZES, PER_MAP_COLOR_MARKS_SHOWN, SOMETHING_HINTS_SHOWN
 
 
 COLORS_INTS = [
-    [256, 256, 256],
-    [ 49, 206, 191],
-    [200,  43, 212],
-    [206, 224,  31]
+    [255, 255, 255],
+    [ 50, 200, 190],
+    [200,  40, 210],
+    [200, 220,  30]
 ]
 
 TILE_ITEM_TYPES_COLORS = [
@@ -38,6 +39,10 @@ class GameGUI1:
         self.clock = pygame.time.Clock()
         self.is_running = True
 
+        # visible clues:
+        self._generate_color_marks_to_show()
+        self._generate_somethings_to_show()
+
         # visual notes/marks:
         self.mazes_color_map = np.zeros((NUM_OF_MAZES, *self.grid_shape), dtype=int)
         self.mazes_markers_map = np.zeros((NUM_OF_MAZES, *self.grid_shape), dtype=int)
@@ -47,6 +52,30 @@ class GameGUI1:
         self._create_mazes_panel()
         self._create_maze_control_panel()
     
+    def _generate_color_marks_to_show(self):
+        self.color_marks_to_show = np.zeros((NUM_OF_MAZES, *self.grid_shape), dtype=int)
+        random.seed(self.game.seed)
+        for maze_id, maze in enumerate(self.game.mazes):
+            marks = maze.get_all_color_marks()
+            random.shuffle(marks)
+            marks_to_show = marks[:PER_MAP_COLOR_MARKS_SHOWN]
+            deceptive_mark_id = random.randrange(PER_MAP_COLOR_MARKS_SHOWN)
+            for idx, ((i, j), tc) in enumerate(marks_to_show):
+                if idx != deceptive_mark_id:
+                    self.color_marks_to_show[maze_id, i, j] = tc.value
+                else:
+                    self.color_marks_to_show[maze_id, i, j] = tc.value+1 if tc.value != 3 else 1
+
+    def _generate_somethings_to_show(self):
+        self.somethings_to_show = np.zeros((NUM_OF_MAZES, *self.grid_shape), dtype=int)
+        random.seed(self.game.seed)
+        for maze_id, maze in enumerate(self.game.mazes):
+            hints = maze.get_all_things()
+            random.shuffle(hints)
+            hints_to_show = hints[:SOMETHING_HINTS_SHOWN]
+            for ((i, j), _) in hints_to_show:
+                self.somethings_to_show[maze_id, i, j] = 1
+
     def _create_mazes_panel(self):
         self.mazes_panel = Panel(
             (20, 20), 
@@ -82,14 +111,19 @@ class GameGUI1:
                         fill_color = (10, 10, 10)
                 else:
                     fill_color = (120, 120, 120)
-                
-                # solid rect:
+                # tile itself:
                 pygame.draw.rect(self.surface, fill_color, 
                     pygame.rect.Rect(20 + SKIP_SIZE + (SKIP_SIZE + TILE_SIZE)*j, 
                                     SKIP_SIZE + 20 + (SKIP_SIZE + TILE_SIZE)*i, TILE_SIZE, TILE_SIZE),
                     border_radius=2
                 )
                 # border rect:
+                if (col_ind:=self.color_marks_to_show[self.chosen_maze_idx, i, j]) > 0:
+                    pygame.draw.rect(self.surface, COLORS_INTS[col_ind],
+                        pygame.rect.Rect(24 + SKIP_SIZE + (SKIP_SIZE + TILE_SIZE)*j, 
+                                        SKIP_SIZE + 24 + (SKIP_SIZE + TILE_SIZE)*i, TILE_SIZE-8, TILE_SIZE-8),
+                        width=5
+                    )
                 if (self.mazes_boolean_map[self.chosen_maze_idx, i, j]):
                     pygame.draw.rect(self.surface, (150, 150, 150),
                         pygame.rect.Rect(21 + SKIP_SIZE + (SKIP_SIZE + TILE_SIZE)*j, 
@@ -110,6 +144,11 @@ class GameGUI1:
                                         SKIP_SIZE + 30 + (SKIP_SIZE + TILE_SIZE)*i, TILE_SIZE-20, TILE_SIZE-20),
                         border_radius=3
                     )
+                if (self.somethings_to_show[self.chosen_maze_idx, i, j]):
+                    self.surface.blit(
+                        FONT_NORM.render('?!', False, BLACK),
+                        pygame.rect.Rect(30 + SKIP_SIZE + (SKIP_SIZE + TILE_SIZE)*j, 
+                                        SKIP_SIZE + 30 + (SKIP_SIZE + TILE_SIZE)*i, TILE_SIZE-20, TILE_SIZE-20))
                 
 
     def maze_tile_hovering(self, pos):
@@ -122,6 +161,9 @@ class GameGUI1:
     def update_gui(self, pos):
         self.mazes_panel.update(pos)
         self.maze_control_panel.update(pos)
+        for maze_id_str, choose_maze_btn in self.maze_control_panel.gui_objects.items():
+            choose_maze_btn.set_frame_color(
+                '#00FF00' if int(maze_id_str) == self.chosen_maze_idx else '#FFFFFF')
         self.draw_maze(self.chosen_maze_idx)
 
     def run(self):
@@ -143,6 +185,11 @@ class GameGUI1:
                             coord = self.maze_tile_hovering(pos)
                             self.mazes_boolean_map[self.chosen_maze_idx, coord[0], coord[1]] = \
                                 1 - self.mazes_boolean_map[self.chosen_maze_idx, coord[0], coord[1]]
+                    elif event.key == pygame.K_q:
+                        #? debug
+                        if self.mazes_panel.clicked():
+                            coord = self.maze_tile_hovering(pos)
+                            print('deb', coord, self.game.mazes[self.chosen_maze_idx][coord[0]][coord[1]])
                 elif event.type == pygame.MOUSEBUTTONUP:
                     if self.maze_control_panel.clicked():
                         obj_clicked = self.maze_control_panel.object_clicked()
